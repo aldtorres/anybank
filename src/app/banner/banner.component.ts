7762a98c3f824cd9c2a3a1cd35ead49ec352cb69
torrestor <torres.jovem@gmail.com>
2025-11-13 16:43:49 -0300

    usar PIPES Angular - DatePipe - Personalizar datas

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,11 +1,15 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-banner',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
+
+  //propriedades
+  prpDataAtual = new Date();
 
 }
