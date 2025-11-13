b38d19e851c7871bf24c4377aa19d1bc2e9e3bb6
torrestor <torres.jovem@gmail.com>
2025-11-13 17:06:23 -0300

    Criação componente - boas vindas - conta

---
+++ src/app/banner/boas-vindas/boas-vindas.component.ts
@@ -0,0 +1,14 @@
+import { DatePipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boas-vindas',
+  imports: [DatePipe],
+  templateUrl: './boas-vindas.component.html',
+  styleUrl: './boas-vindas.component.css'
+})
+export class BoasVindasComponent {
+
+  //propriedades
+  prpDataAtual = new Date();
+}
