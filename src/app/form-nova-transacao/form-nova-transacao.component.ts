53678df9c40db7399df1802e1618e3a8bdde4c39
torrestor <torres.jovem@gmail.com>
2025-11-13 18:13:03 -0300

    two-way data biding - vincular atributo a váriavel = useState do React

--- src/app/form-nova-transacao/form-nova-transacao.component.ts
+++ src/app/form-nova-transacao/form-nova-transacao.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-nova-transacao.component.html',
   styleUrl: './form-nova-transacao.component.css'
 })
 export class FormNovaTransacaoComponent {
 
+    valorTransacao ="";
+
+    //
+    aoSubmeter(){
+      console.log('submetido!');
+      console.log(this.valorTransacao);
+    }
 }
