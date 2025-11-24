f4a597367e732fed434a26261a86c48edab697e4
torrestor <torres.jovem@gmail.com>
2025-11-24 11:47:36 -0300

    enum  ->  KeyValuePipe  ->  @for  ->  em enum

--- src/app/form-nova-transacao/form-nova-transacao.component.ts
+++ src/app/form-nova-transacao/form-nova-transacao.component.ts
@@ -1,10 +1,11 @@
 import { Component, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TipoTransacao, Transacao } from '../model/transacao';
+import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'app-form-nova-transacao',
-  imports: [FormsModule],
+  imports: [FormsModule, KeyValuePipe],
   templateUrl: './form-nova-transacao.component.html',
   styleUrl: './form-nova-transacao.component.css'
 })
@@ -12,6 +13,8 @@ export class FormNovaTransacaoComponent {
 
     valorTransacao ="";
     tipoTransacao ="";
+
+    tipoTransacaoEnum = TipoTransacao;
 
     //outPut
     handleTransacaoCriada = output<Transacao>();
