4d47f840acef81e4c487b52d04074bd627a0ab1a
torrestor <torres.jovem@gmail.com>
2025-11-24 12:29:07 -0300

    componente Extrato

---
+++ src/app/extrato/transacao/transacao.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { Transacao } from '../../model/transacao';
+
+@Component({
+  selector: 'app-transacao',
+  imports: [],
+  templateUrl: './transacao.component.html',
+  styleUrl: './transacao.component.css'
+})
+export class TransacaoComponent {
+
+  transacao = input.required<Transacao>()
+
+}
