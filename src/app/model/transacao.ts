52df54c0d5eb438ce971037375749fb90eeced47
torrestor <torres.jovem@gmail.com>
2025-11-24 12:53:57 -0300

    FORMATAR DATA  ->  DatePipe  -  {{ transacao().data | date: dd/MM/yyyy }}

--- src/app/model/transacao.ts
+++ src/app/model/transacao.ts
@@ -3,6 +3,7 @@ import { nanoid } from "nanoid";
 export class Transacao {
     
     readonly id = nanoid();
+    readonly data = new Date();
     
     constructor(
         public readonly tipo: TipoTransacao, 
